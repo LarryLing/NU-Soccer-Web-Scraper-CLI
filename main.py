a216187e50131c7cbf74007519a1e9116b8c62a3
CS376-Freestyle-Game <larryling.main@gmail.com>
2025-05-13 11:34:14 -0500

    Add settings.json for changing output path and ignore columns

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import ChromiumOptions
 from scrape import print_roster, print_schedule
 from utils import get_team_data, prompt
+import json
 
+# Initialize the Chrome WebDriver
 service = Service(executable_path="chromedriver.exe")
 chrome_options = ChromiumOptions()
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
+# Configure PDFKit by setting the path to wkhtmltopdf
 wkhtmltopdf_path = "wkhtmltopdf\\bin\\wkhtmltopdf.exe"
 pdfkit_config = pdfkit.configuration(wkhtmltopdf=wkhtmltopdf_path)
+
+# Read JSON settings file
+with open("settings.json", "r") as file:
+    settings = json.load(file)
 
 team_name = prompt()
 
@@ -26,10 +33,10 @@ while (team_name != "Exit"):
         continue
 
     url = f"{team_data["base_url"]}/sports/mens-soccer/roster/print"
-    print_roster(team_name, driver, url, pdfkit_config)
+    print_roster(team_name, driver, url, pdfkit_config, settings)
 
     url = f"{team_data["base_url"]}/sports/mens-soccer/schedule?view=table&print=auto"
-    print_schedule(team_name, driver, url, pdfkit_config)
+    print_schedule(team_name, driver, url, pdfkit_config, settings)
 
     team_name = prompt()
 
--- scrape.py
+++ scrape.py
@@ -1,13 +1,15 @@
 import pdfkit
 from utils import create_html_table, process_table
 
-def print_roster(university_name, driver, url, pdfkit_config, ignore_columns=["Twitter", "Instagram", "Major"]):
+def print_roster(university_name, driver, url, pdfkit_config, settings):
     """
     Print the roster page to a PDF file.
     """
 
     title = f"{university_name} Mens Soccer Roster"
-    output_path = f"output/{university_name}_roster.pdf"
+    output_path = f"{settings["output"]["path"]}\\{university_name}_roster.pdf"
+
+    ignore_columns = settings["roster"]["ignore_columns"]
 
     columns, rows = process_table(driver, url, ignore_columns=ignore_columns)
 
@@ -15,13 +17,15 @@ def print_roster(university_name, driver, url, pdfkit_config, ignore_columns=["T
 
     pdfkit.from_string(html, output_path, configuration=pdfkit_config)
 
-def print_schedule(university_name, driver, url, pdfkit_config, ignore_columns=["Links"]):
+def print_schedule(university_name, driver, url, pdfkit_config, settings):
     """
     Print the schedule page to a PDF file.
     """
 
     title = f"{university_name} Mens Soccer Schedule"
-    output_path = f"output/{university_name}_schedule.pdf"
+    output_path = f"{settings["output"]["path"]}\\{university_name}_schedule.pdf"
+
+    ignore_columns = settings["schedule"]["ignore_columns"]
 
     columns, rows = process_table(driver, url, ignore_columns=ignore_columns)
 
